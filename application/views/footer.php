53377fd72230c214f9691c0ea035a5269959154c
Marcelo Silveira <marcelo.boemeke@gmail.com>
2016-11-17 12:07:45 -0200

    Bug-fix: Sistema/Contato não inseria máscara nos inputs; load duplicado dos arquivos .js

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,16 +1,5 @@
 <!-- Importar JS aqui -->
 <script src="<?php echo base_url('js/bootstrap.min.js')?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/jquery.flot.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/jquery.flot.pie.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/jquery.flot.orderBars.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/jquery.flot.time.min.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/date.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/jquery.flot.spline.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/jquery.flot.stack.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/curvedLines.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('js/flot/jquery.flot.resize.js'); ?>"></script>
-<script src="<?php echo base_url('js/progressbar/bootstrap-progressbar.min.js'); ?>"></script>
-<script src="<?php echo base_url('js/nicescroll/jquery.nicescroll.min.js'); ?>"></script>
 <!-- icheck -->
 <script src="<?php echo base_url('js/icheck/icheck.min.js'); ?>"></script>
 <!-- tags -->
