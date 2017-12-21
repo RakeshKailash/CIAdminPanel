b6b8331bce32307ce060a6f696ec947f1b3c526d
Marcelo Silveira <marcelo.boemeke@gmail.com>
2017-12-21 14:49:46 -0200

    Editar legenda da Home (e ajuste quando não há capa para a Home)

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -4,7 +4,7 @@ $info['cabecalho'] = array('menu' => 'site/menu', 'header' => 'site');
 $info['itens'] = $itens;
 $this->load->view('header', $info);
 ?>
-<section id="hero-area" style="height: 100%; background-size: cover; background-position: center; background-image: url(<?=base_url($secao_info->caminho);?>);">
+<section id="hero-area" style="height: 100%; <?php if (! empty($secao_info->caminho)):?>background-size: cover; background-position: center; background-image: url(<?=base_url($secao_info->caminho);?>); <?php endif; ?>">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 text-center">
@@ -13,19 +13,19 @@ $this->load->view('header', $info);
 					<!-- Slider -->
 					<section class="cd-intro">
 						<h1 class="wow fadeInUp animated cd-headline slide" data-wow-delay=".4s" >
-							<span>LOREM, IPSUM DOLOR SIT AMET</span><br>
-							<span class="cd-words-wrapper">
+							<span><?=$secao_info->conteudo?></span>
+							<!-- <span class="cd-words-wrapper">
 								<b class="is-visible">CONSECTETUR</b>
 								<b>ADIPISCING</b>
 								<b>ELIT</b>
-							</span>
+							</span> -->
 						</h1>
 					</section> <!-- cd-intro -->
 					<!-- /.slider -->
-					<h2 class="wow fadeInUp animated" data-wow-delay=".6s" >
+					<!-- <h2 class="wow fadeInUp animated" data-wow-delay=".6s" >
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...
-					</h2>
-					<a class="btn-lines dark light wow fadeInUp animated btn btn-default btn-green" data-wow-delay=".9s" href="<?=base_url('site/servicos')?>">Saiba Mais</a>
+					</h2> -->
+					<!-- <a class="btn-lines dark light wow fadeInUp animated btn btn-default btn-green" data-wow-delay=".9s" href="<?=base_url('site/servicos')?>">Saiba Mais</a> -->
 
 				</div>
 			</div>
